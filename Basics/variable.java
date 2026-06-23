6ce2b26d6a76979793e7ddfed4d24b2f33043cb1
Hemal2005 <hemalvelamala14@gmail.com>
2026-06-23 22:08:32 +0530

    Learning datatypes,variables,syntax in java

---
+++ Basics/variable.java
@@ -0,0 +1,10 @@
+class Varaible {
+    public static void main(){
+        int num1 = 5;
+        int num2 = 4;
+        System.out.println(num1 + num2);
+    }
+}
+
+//dataType variableName = value;(Syntax of Assiigning a varaible)
+//local variable,instance varaible, global variable,etc...
