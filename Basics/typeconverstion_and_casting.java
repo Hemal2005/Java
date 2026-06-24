5f5ad12788a8729be642bf09bf4654ab993fd35c
Hemal2005 <hemalvelamala14@gmail.com>
2026-06-24 23:01:45 +0530

    TypeConversion and Casting

---
+++ Basics/typeconverstion_and_casting.java
@@ -0,0 +1,15 @@
+class Type{
+    public static void main(){
+        
+        int a = 257;
+        byte k = (byte) a; //Type casting (i.e expilicit casting)(op:1 because perfoms mod by range amd gives remainder)
+        int b = 50;
+        double c = b; //Type conversion(impilicit casting)
+
+        float f = 5.6f;
+        int t = (int) f;//op:5
+        System.out.println(k);
+        System.out.println(c);
+
+    }
+}
